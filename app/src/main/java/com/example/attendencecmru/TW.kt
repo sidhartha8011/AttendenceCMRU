29f53bb00420f4d39622e8eb7ae906c5f9a9f4ac
sidhartha8011 <sidharthparasramka@gmail.com>
2023-01-13 23:39:59 +0530

    commit

---
+++ app/src/main/java/com/example/attendencecmru/TW.kt
@@ -0,0 +1,36 @@
+package com.example.attendencecmru
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.attendencecmru.ui.main.SectionsPagerAdapter
+import com.example.attendencecmru.databinding.ActivityTwBinding
+
+class TW : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTwBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityTwBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = binding.fab
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
